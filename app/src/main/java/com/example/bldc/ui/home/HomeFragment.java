90d335cb6394cc797294a23f4de6ee1bd6a55b65
Rowan Webster <rowanqwebster@gmail.com>
2021-02-01 23:12:17 -0700

    Started work on bluetooth pairing

--- app/src/main/java/com/example/bldc/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/bldc/ui/home/HomeFragment.java
@@ -1,12 +1,20 @@
 package com.example.bldc.ui.home;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,9 +24,18 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.bldc.R;
 
+import java.util.ArrayList;
+import java.util.Set;
+
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private Button btnPaired;
+    private ListView devicelist;
+
+    private BluetoothAdapter myBluetooth = null;
+    private Set<BluetoothDevice> pairedDevices;
+    public static String EXTRA_ADDRESS = "device_address";
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
@@ -31,6 +48,7 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
         /*
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -43,4 +61,58 @@ public class HomeFragment extends Fragment {
         setHasOptionsMenu(true);
         return root;
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState)
+    {
+        Toast toast = Toast.makeText(getActivity(), "Hi", Toast.LENGTH_SHORT);
+        toast.show();
+
+        btnPaired = (Button) getView().findViewById(R.id.button);
+        devicelist = (ListView) getView().findViewById(R.id.listView);
+
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();
+        if ( myBluetooth==null ) {
+            Toast.makeText(getActivity(), "Bluetooth device not available", Toast.LENGTH_LONG).show();
+        } else if ( !myBluetooth.isEnabled() ) {
+            Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnBTon, 1);
+        }
+
+       btnPaired.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               pairedDevicesList();
+           }
+       });
+    }
+
+    private void pairedDevicesList () {
+        pairedDevices = myBluetooth.getBondedDevices();
+        ArrayList list = new ArrayList();
+
+        if ( pairedDevices.size() > 0 ) {
+            for ( BluetoothDevice bt : pairedDevices ) {
+                list.add(bt.getName().toString() + "\n" + bt.getAddress().toString());
+            }
+        } else {
+            Toast.makeText(getActivity(), "No Paired Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
+        }
+
+        //final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        //devicelist.setAdapter(adapter);
+        devicelist.setOnItemClickListener(myListClickListener);
+    }
+
+    private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String info = ((TextView) view).getText().toString();
+            String address = info.substring(info.length()-17);
+
+            //Intent i = new Intent(HomeFragment.this, ledControl.class);
+            //i.putExtra(EXTRA_ADDRESS, address);
+            //startActivity(i);
+        }
+    };
 }
